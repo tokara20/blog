f7ca0eec7a3af2333552a5f858fa493c885dacd7
Tokara <tokara20@yandex.com>
2016-12-15 05:56:13 +0000

    reanme feature to features and made spec messages to base on an actor

--- spec/features/verify_home_page_spec.rb
+++ spec/features/verify_home_page_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
-RSpec.feature "Verify Home Page" do
+RSpec.feature "Visitor visits Home Page" do
     
   before do
     visit root_path      
   end
     
-  scenario "has Home and New Post links" do
+  scenario "and it has Home and New Post links" do
     #visit root_path
     expect(page).to have_content('Home')
     expect(page).to have_content('New Post')
   end
   
-  scenario "Home link links to Home Page" do
+  scenario "it has a link to the Home Page" do
     expect(page).to have_link('Home', href: root_path)
   end
   
-  scenario "New Post links to New Post Page" do
+  scenario "it has a link to the New Post Page" do
     expect(page).to have_link('New Post', href: new_post_path)
   end
 end
